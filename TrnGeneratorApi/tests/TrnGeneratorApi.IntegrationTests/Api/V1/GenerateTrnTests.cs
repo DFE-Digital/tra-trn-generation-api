aec26a762c086f5956684790a6a4042cece9a748
Andrew Horth <andrew@horth.co.uk>
2022-11-11 15:33:17 +0000

    Added more integration tests

--- TrnGeneratorApi/tests/TrnGeneratorApi.IntegrationTests/Api/V1/GenerateTrnTests.cs
+++ TrnGeneratorApi/tests/TrnGeneratorApi.IntegrationTests/Api/V1/GenerateTrnTests.cs
@@ -1,5 +1,6 @@
 namespace TrnGeneratorApi.IntegrationTests.Api.V1;
 
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 public class GenerateTrnTests : IClassFixture<WebApplicationFactory<Program>>
@@ -22,5 +23,36 @@ public class GenerateTrnTests : IClassFixture<WebApplicationFactory<Program>>
 
         // Assert
         Assert.Equal(StatusCodes.Status401Unauthorized, (int)response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Post_WithInvalidApiKey_Returns401Unauthorised()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "xyz");
+
+        // Act
+        var response = await client.PostAsync("/api/v1/trn", null);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status401Unauthorized, (int)response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Post_WithValidApiKey_Returns200OKAndTrn()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "12345");
+
+        // Act
+        var response = await client.PostAsync("/api/v1/trn", null);
+
+        // Assert
+        Assert.Equal(StatusCodes.Status200OK, (int)response.StatusCode);
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(result);
+        Assert.True(int.TryParse(result, out int value), "Result is an integer");
     }
 }
